a18729257a3b7fa5e7fa7ef1c15f0b31ef719f54
Adam Fowler <adam@adamfowler.org>
2021-12-12 10:03:18 +0000

    Part of #41. Updated filtering and rssi over time chart
    Signed-off-by: Adam Fowler <adam@adamfowler.org>

--- SelfCalibration.R
+++ SelfCalibration.R
@@ -20,9 +20,9 @@ library(stringr)
 basedir <- "./sample-output/2020-08-11-cx-47"
 phonedir <- "Pixel3XL"
 
-filtertimemin <- as.POSIXct(paste("2021-11-16", "01:00:00"), format="%Y-%m-%d %H:%M:%S")
+filtertimemin <- as.POSIXct(paste("2021-11-14", "16:00:00"), format="%Y-%m-%d %H:%M:%S")
 filtertimemin
-filtertimemax <- as.POSIXct(paste("2021-11-16", "23:00:00"), format="%Y-%m-%d %H:%M:%S")
+filtertimemax <- as.POSIXct(paste("2021-11-15", "00:30:00"), format="%Y-%m-%d %H:%M:%S")
 filtertimemax
 
 cestart <- as.POSIXct(paste("2021-11-16", "01:00:00"), format="%Y-%m-%d %H:%M:%S")
@@ -42,29 +42,32 @@ csvdata <- tryCatch({
   #  # error handler picks up where error was generated 
   print(paste("Read.table didn't work for contacts!:  ",err))
 })
+head(csvdata)
 
 # We only care about measures (and their RSSI and TxPower values)
 # i.e. the didMeasure calls (for ALL nearby devices, prefiltering)
 measures <- dplyr::filter(csvdata,measure==3)
 head(measures)
 measures <- dplyr::select(measures,c("time","id","data"))
-measures <- dplyr::distinct(measures)
+#measures <- dplyr::distinct(measures)
 names(measures) <- c("time","macuuid","data")
 head(measures)
 
 
 # Filter by time
 measures$t <- as.POSIXct(measures$time, format="%Y-%m-%d %H:%M:%S")
-#measures <- dplyr::filter(measures,t>=filtertimemin)
-#measures <- dplyr::filter(measures,t<=filtertimemax)
+measures <- dplyr::filter(measures,t>=filtertimemin)
+measures <- dplyr::filter(measures,t<=filtertimemax)
 head(measures)
 
 # Now extract RSSI and txPower (if present)
 rssiAndTxPowerPattern <- "RSSI:(-[0-9]+\\.0)(,TxPower:([0-9]+))?"
-matches <- str_match(measures$data,rssiAndTxPowerPattern)
-head(matches)
+#matches <- str_match(measures$data,rssiAndTxPowerPattern)
+#head(matches)
 measures$rssi <- str_match(measures$data,rssiAndTxPowerPattern)[,2]
 measures$txpower <- str_match(measures$data,rssiAndTxPowerPattern)[,4]
+#measures %>%
+#  dplyr::mutate(rssi = str_match(measures$data,rssiAndTxPowerPattern)[,2])
 head(measures)
 
 # Filter out those without RSSI
@@ -72,9 +75,15 @@ measures <- dplyr::filter(measures,!is.na(rssi))
 measures$rssiint <- as.numeric(measures$rssi)
 head(measures)
 
+# Filter invalid RSSIs (Same as we do in the Herald analysis API)
+measures <- dplyr::filter(measures,rssiint>-100)
 
-chartWidth <- 1200
-chartHeight <- 800
+#measures <- dplyr::select(measures,c("time","id","rssiint","txpower"))
+#names(measures) <- c("time","macuuid","rssiint","txpower")
+#head(measures)
+
+chartWidth <- 400
+chartHeight <- 300
 
 # Graph 1a&b - Show RSSI frequencies by macuuid across whole time period
 # Note: As devices rotate mac address, some devices will be the same but 
@@ -101,9 +110,13 @@ p
 ggsave(paste(basedir,"/",phonedir,"-rssi-density.png", sep=""), width = chartWidth, height = chartHeight, units = "mm")
 
 # Graph 2 - Smoothed line of rssi over time (3 degrees of freedom)
-p <- ggplot(measures, aes(x=t,y=rssi,color=macuuid)) +
+p <- ggplot(measures, aes(x=t,y=rssiint,color=macuuid)) +
   geom_point(show.legend = F) +
-  geom_smooth(method="lm", formula=y ~ poly(x,3), show.legend = F)
+  labs(x="Time",
+       y="RSSI",
+       title="RSSI detected over time",
+       subtitle="Some phones may be duplicates")
+#  + geom_smooth(method="lm", formula=y ~ poly(x,3), show.legend = F)
 #  geom_smooth(method="loess")
 p
 ggsave(paste(basedir,"/",phonedir,"-rssi-over-time.png", sep=""), width = chartWidth, height = chartHeight, units = "mm")
